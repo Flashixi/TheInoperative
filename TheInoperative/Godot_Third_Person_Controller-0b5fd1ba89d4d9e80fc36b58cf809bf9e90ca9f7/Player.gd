acd4248deecf618641c7770861676d5dc9502506
Flashixi <82478073+Flashixi@users.noreply.github.com>
2021-09-17 12:49:50 +1200

    Add files via upload

--- TheInoperative/Godot_Third_Person_Controller-0b5fd1ba89d4d9e80fc36b58cf809bf9e90ca9f7/Player.gd
+++ TheInoperative/Godot_Third_Person_Controller-0b5fd1ba89d4d9e80fc36b58cf809bf9e90ca9f7/Player.gd
@@ -6,6 +6,9 @@ const MOUSE_SENS = 0.5
 onready var anim_player = $AnimationPlayer
 onready var raycast = $RayCast
 
+func kill():
+	get_tree().reload_current_scene()
+
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	yield(get_tree(), "idle_frame")
@@ -23,9 +26,21 @@ func _process(delta):
 
 func _physics_process(delta):
 	var move_vec = Vector3()
-	if Input.is_action_pressed("move_forwards"):
+	if Input.is_action_pressed("forward"):
 		move_vec.z -= 1
-	if Input.is_action_pressed("move_backwards"):
+	if Input.is_action_pressed("backward"):
 		move_vec.z += 1
-	if Input.is_action_pressed("move_left"):
+	if Input.is_action_pressed("left"):
 		move_vec.x -= 1
+	if Input.is_action_pressed("right"):
+		move_vec.x += 1
+	move_vec = move_vec.normalized()
+	move_vec = move_vec.rotated(Vector3(0, 1, 0), rotation.y)
+	move_and_collide(move_vec * MOVE_SPEED * delta)
+	
+	if Input.is_action_just_pressed("shoot") and !anim_player.is_playing():
+		anim_player.play("shoot")
+		var coll = raycast.get_collider()
+		if raycast.is_colliding() and coll.has_method("kill"):
+			coll.kill()
+	
